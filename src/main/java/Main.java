4ea4ba12dce3b29425a9487b261960398595faf3
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-05-13 16:25:51 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Série favorita: Mr robots");
+    System.out.println("");
+    System.out.println("Sinopse: Mr. Robot é uma série de televisão que mergulha profundamente na mente perturbada e complexa de Elliot Alderson, um hacker habilidoso e socialmente recluso que sofre de transtornos mentais. A série explora temas de alienação, identidade, poder e controle em uma sociedade moderna dominada por tecnologia e corrupção.");
+    System.out.println("");
+    System.out.println("Trailer da Série:");
+    System.out.println("");
+    System.out.println("https://www.youtube.com/watch?v=8qZYW_1hj2g"); 
+  }
+}
